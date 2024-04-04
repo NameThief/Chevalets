ba1b6f32f55f03e2955dd57226788c5e106c7f18
Adam Lasmi <heisvexiane@gmail.com>
2024-04-04 10:58:49 +0200

    remplacer $pdf par $chevaletpdfmaker

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -6,18 +6,13 @@ use App\Model\Emargement;
 use App\Service\ChevaletPDFMaker;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use App\Model\Chevalet;
 use PhpOffice\PhpSpreadsheet\IOFactory;
-use Symfony\Component\Filesystem\Filesystem;
-use FPDF;
 use Symfony\Component\HttpFoundation\StreamedResponse;
-use PhpOffice\PhpSpreadsheet\Reader\Xls;
 
 class UploadController extends AbstractController
 {
@@ -42,8 +37,8 @@ class UploadController extends AbstractController
             $data = $form->getData();
 
             if ($data['document'] == 'chevalets') {
-                // Créer un objet de type FPDF
-                $pdf = new FPDF('P', 'mm', 'A4');
+
+
                 // Créer un objet de type ChevaletPDFMaker
                 $chevaletPDFMaker = new ChevaletPDFMaker();
                 // Charger dans le fichier excel les feuilles "Animateurs" et "Participants"
@@ -64,7 +59,7 @@ class UploadController extends AbstractController
                             $chevalet->setPrenom(($row['C']));
                             $chevalet->setFonction(($row['E']));
                             // Ajout du chevalet au PDF en utilisant l'objet $chevaletMaker
-                            $chevaletPDFMaker->addChevaletToPDF($pdf, $chevalet);
+                            $chevaletPDFMaker->addChevaletToPDF($chevalet);
                         } // Le reader arrête de lire le fichier excel quand il rencontre une ligne vide
                         else {
                             break;
@@ -72,15 +67,15 @@ class UploadController extends AbstractController
                     }
                 }
                 // Générer le PDF et le renvoyer en réponse
-                return new StreamedResponse(function () use ($pdf) {
-                    echo $pdf->Output();
+                return new StreamedResponse(function () use ($chevaletPDFMaker) {
+                    echo $chevaletPDFMaker->getOutput();
                 }, 200, [
                     'Content-Type' => 'application/pdf',
                     'Content-Disposition' => 'attachment; filename="chevalets.pdf"'
                 ]);
             } elseif ($data['document'] == 'emargement') {
                 // Chemin vers le fichier Excel
-                $pdf = new FPDF();
+
                 $excelFilePath = $data['fichier']->getPathname();
 
                 // Charger le fichier Excel
@@ -147,9 +142,3 @@ class UploadController extends AbstractController
         ]);
     }
 }
-
-
-
-
-
-
